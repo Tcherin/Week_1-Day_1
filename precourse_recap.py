f792a227784ed255d02e5f40ddb64ba80f43e0c9
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-03-20 15:38:11 +0000

    first homework

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+name = "Richard"
+age = 28
+home = "67 Drumoyne Circus"
+
+print(name + " is " + str(age) + " years old and lives at " + str(home))
+
+number_1 = 679
+number_2 = 472
+
+sum = number_1 + number_2
+
+print(sum)
